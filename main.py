954483dda1b802e6f1d1d62f72a345bc67b0daff
Liam Doyle <l.doyler04@gmail.com>
2024-03-07 11:17:40 +0000

    More basic setup

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import pygame
 
 pygame.init()
 
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
@@ -12,5 +22,13 @@ running = True
 while running:
     for event in pygame.event.get():
 
-        if event.type == pygame.QUIT:
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        if event.type == QUIT:
             running = False
+            
+    pressed_keys = pygame.key.get_pressed() # makes a dictionary of all the keys that are currently pressed down
+
+    pygame.display.flip() # refreshes the display every frame
